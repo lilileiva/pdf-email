325865fdbdf8cb63108dfac6e9a125de9edd2926
Liliana Leiva <94813118+lilileiva@users.noreply.github.com>
2024-06-07 20:24:31 -0300

    add smtp service

--- pdf_email/main.py
+++ pdf_email/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
+from .router import router
+
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.include_router(router)
---
+++ pdf_email/services/smtp.py
@@ -0,0 +1,26 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+
+
+class SMTPService:
+    def __init__(
+        self,
+        host: str = os.getenv("MAILTRAP_HOST"),
+        port: int = os.getenv("MAILTRAP_PORT"),
+        user: str = os.getenv("MAILTRAP_USER"),
+        password: str = os.getenv("MAILTRAP_PASSWORD"),
+        sender_email: str = os.getenv("MAILTRAP_SENDER"),
+        local_hostname: str = "localhost",
+    ):
+        self.client = smtplib.SMTP(host=host, port=port, local_hostname=local_hostname)
+        self.client.starttls()
+        self.client.login(user=user, password=password)
+        self.send_email = sender_email
+
+    def send_email(self, receiver_email: str, subject: str, content: str):
+        message = MIMEText(content, "plain")
+        message["Subject"] = subject
+        message["From"] = self.sender_email
+        message["To"] = receiver_email
+        self.client.sendmail(from_addr=self.sender_email, to_addrs=receiver_email, msg=message.as_string())
