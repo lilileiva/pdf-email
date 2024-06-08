1bb2b939b2351eef3eec49705dc151773df9c721
Liliana Leiva <94813118+lilileiva@users.noreply.github.com>
2024-06-08 17:25:04 -0300

    add description to upload endpoint

--- pdf_email/responses.py
+++ pdf_email/responses.py
@@ -4,3 +4,4 @@ UPLOAD_RESPONSE = {
 }
 
 upload_responses = {200: UPLOAD_RESPONSE[200], 500: UPLOAD_RESPONSE[500]}
+upload_description = "Send email to the given address with the text of the given PDF file."
--- pdf_email/router.py
+++ pdf_email/router.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, Depends
 from pdf_email.schemas.serializers import UploadSerializer
 from pdf_email.schemas.validators import UploadValidator
-from pdf_email.responses import upload_responses
+from pdf_email.responses import upload_responses, upload_description
 from pdf_email.utils import send_file
 
 router = APIRouter()
 
 
-@router.post("/upload", tags=["upload"], responses=upload_responses)
+@router.post("/upload", tags=["upload"], responses=upload_responses, description=upload_description)
 async def upload_file(body: UploadValidator = Depends()) -> UploadSerializer:
     validated_fields = UploadValidator.validate_fields(body.__dict__)
     email = validated_fields["email"]
