9f8c3473e4c94dcf33a99d4e5e8311922c9b8561
Liliana Leiva <94813118+lilileiva@users.noreply.github.com>
2024-06-08 15:55:38 -0300

    separate requirements + update docs

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM python:3.11-alpine
 
 RUN apk update
 
-COPY requirements.txt requirements.txt
-RUN pip install -r requirements.txt
+COPY requirements requirements
+RUN pip install -r requirements/local.txt
 
 WORKDIR /app
 
--- pdf_email/router.py
+++ pdf_email/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, File, Form, UploadFile
+from fastapi import APIRouter, File, Form, UploadFile, Response
 from pdf_email.schemas.serializers import UploadSerializer
 from pdf_email.schemas.validators import UploadValidator
 from pdf_email.responses import upload_responses
@@ -8,7 +8,7 @@ router = APIRouter()
 
 
 @router.post("/upload", tags=["upload"], responses=upload_responses)
-async def upload_file(email: str = Form(...), file: UploadFile = File(...)):
+async def upload_file(email: str = Form(...), file: UploadFile = File(...)) -> UploadSerializer:
     body = {"email": email, "file": file}
     validated_fields = UploadValidator(**body).model_dump()
     email = validated_fields["email"]
@@ -16,5 +16,4 @@ async def upload_file(email: str = Form(...), file: UploadFile = File(...)):
     send_file(file, email)
     response = {"success": True, "message": f"The email has been successfully sent to {email}"}
     validated_response = UploadSerializer.model_validate(response)
-    response = validated_response.model_dump()
-    return response
+    return validated_response
