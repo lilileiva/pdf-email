a3704b7419e72d54618b46128e4f3b2783fe2ce9
Liliana Leiva <94813118+lilileiva@users.noreply.github.com>
2024-06-08 17:08:24 -0300

    add CustomException class

--- pdf_email/exceptions.py
+++ pdf_email/exceptions.py
@@ -1,17 +1,22 @@
 from fastapi import HTTPException
 
 
-class PDFException(HTTPException):
+class CustomException(HTTPException):
+    """Custom exception"""
+
+    def __init__(self, status_code: int, message: str, error: str):
+        return super().__init__(status_code=status_code, detail={"message": message, "error": error})
+
+
+class PDFException(CustomException):
     """Exception raised when PDF file could not be read"""
 
     def __init__(self, error: str | Exception):
-        return super().__init__(
-            status_code=500, detail={"message": "Error trying to read PDF file.", "error": str(error)}
-        )
+        return super().__init__(status_code=500, message="Error trying to read PDF file.", error=str(error))
 
 
-class EmailException(HTTPException):
+class EmailException(CustomException):
     """Exception raised when email could not be sent"""
 
     def __init__(self, error: str | Exception):
-        return super().__init__(status_code=500, detail={"message": "Error trying to sent email.", "error": str(error)})
+        return super().__init__(status_code=500, message="Error trying to sent email.", error=str(error))
